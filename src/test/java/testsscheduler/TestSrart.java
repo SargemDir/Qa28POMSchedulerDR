51887ed697ba3fbe5e78279586a487d885e395fc
SargemDir <86373554+SargemDir@users.noreply.github.com>
2021-08-22 22:56:20 +0300

    Start test added

---
+++ src/test/java/testsscheduler/TestSrart.java
@@ -0,0 +1,11 @@
+package testsscheduler;
+
+import config.ConfigScheduler;
+import org.testng.annotations.Test;
+
+public class TestSrart extends ConfigScheduler {
+    @Test
+    public void teststart(){
+
+    }
+}
