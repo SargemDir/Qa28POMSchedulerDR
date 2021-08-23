a877bc9f5c8deddc2cb44c2219bdba24129f50ba
SargemDir <86373554+SargemDir@users.noreply.github.com>
2021-08-23 23:55:09 +0300

    LaunchAppTest added

---
+++ src/test/java/testsscheduler/LaunchAppTest.java
@@ -0,0 +1,17 @@
+package testsscheduler;
+
+import config.ConfigScheduler;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pagesscheduler.SplashScreen;
+
+public class LaunchAppTest extends ConfigScheduler {
+
+    @Test
+    public void launchtest(){
+
+        String version = new SplashScreen(driver).getCurrentVersion();
+        Assert.assertEquals(version,"0.0.3");
+    }
+
+}
